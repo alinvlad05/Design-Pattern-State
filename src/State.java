13c7a7cf4478761fdaf26dec7d88224ba60daf8e
Alin Vlad <alinvlad05@gmail.com>
2020-12-26 12:22:03 +0200

    Initial commit

---
+++ src/State.java
@@ -0,0 +1,9 @@
+public interface State {
+    public String gotApplication();
+
+    public String checkApplication();
+
+    public String rentApartment();
+
+    public String dispenseKeys();
+}
---
+++ src/TestAutomat.java
@@ -0,0 +1,15 @@
+public class TestAutomat {
+    Automat automat;
+
+    public static void main(String args[]) {
+        TestAutomat t = new TestAutomat();
+    }
+
+    public TestAutomat() {
+        automat = new Automat(9);
+
+        automat.gotApplication();
+        automat.checkApplication();
+        automat.rentApartment();
+    }
+}
